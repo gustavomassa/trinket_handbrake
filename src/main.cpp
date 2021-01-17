c7ebb6a64f0dddc869dc60372a84f44faab45280
Gustavo Massaneiro <gustavomassa123@gmail.com>
2021-01-17 15:48:24 -0300

    Implemented calibration process based on the min pot position (handbrake off position)

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,32 @@
 
 const int ledPin = 1; // built in red LED
 const int potPin = 3; // PB3
+volatile int minThrottle = 0;
+volatile int maxThrottle = 0;
+volatile int throttleMapped = 0;
+
+int calibrate()
+{
+  int value = 0;
+  for (uint8_t i = 0; i < 100; i++)
+  {
+    value = analogRead(potPin);
+    delay(10);
+  }
+
+  return value;
+}
+
+void blink()
+{
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+}
 
 // the setup routine runs once when you press reset:
 void setup()
@@ -10,14 +36,13 @@ void setup()
   pinMode(ledPin, OUTPUT);
   pinMode(potPin, INPUT);
 
-  /*   // Sinalize setup
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
-  digitalWrite(ledPin, LOW);
-  delay(1000);
-  digitalWrite(ledPin, HIGH); */
-
   TrinketJoystick.begin(); // start the USB device engine and enumerate
+
+  // Start Calibration process
+  blink();
+  minThrottle = calibrate();
+  maxThrottle = (1023 - minThrottle);
+  blink();
 }
 
 // the loop routine runs over and over again forever:
@@ -25,8 +50,13 @@ void loop()
 {
   // ATtiny85: 10 bit ADC
   int throttle = analogRead(potPin);
-  int throttleMapped = map(throttle, 0, 1023, 0, 255);
+
+  if (throttle > minThrottle)
+  {
+    throttle = minThrottle;
+  }
+
+  throttleMapped = map(throttle, minThrottle, maxThrottle, 0, 255);
   TrinketJoystick.setThrottle(throttleMapped);
-  //TrinketJoystick.mouseMove(0, 0, 0b0000000000000000);
   delay(5);
 }
